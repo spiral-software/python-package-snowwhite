604f950802f2de087ebea37369834f975f18dec5
Mike Franusich <mike.franusich@spiralgen.com>
2021-10-20 12:48:56 -0400

    make DFT direction an argument of problem class

--- examples/run-dft.py
+++ examples/run-dft.py
@@ -5,17 +5,17 @@ import numpy as np
 import sys
 
 if len(sys.argv) < 3:
-    print ('Usage: ' + sys.argv[0] + ' xform_size dir (where dir is F/f [forward] or R/r [reverse])' )
+    print ('Usage: ' + sys.argv[0] + ' xform_size dir (where dir is F [forward] or I [inverse])' )
     sys.exit ('missing argument(s)')
 
 n = int ( sys.argv[1] )
-p1 = DftProblem(n)
 if sys.argv[2] == "F" or sys.argv[2] == "f":
     fwd = 1                      ##  standard python transform: 1 ==> forward, -1 reverse
 else:
     fwd = -1
     
-s1 = DftSolver(p1, {SW_OPT_DIRECTION : fwd})
+p1 = DftProblem(n, fwd)
+s1 = DftSolver(p1)
 
 max_diff = 0
 max_diff_py = 0
