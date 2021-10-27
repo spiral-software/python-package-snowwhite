29e9129280d3bb261c7bb4bb27157a6479e2117a
Mike Franusich <mike.franusich@spiralgen.com>
2021-10-27 17:49:23 -0400

    Add MDDFT solver for CPU and CUDA

---
+++ examples/run-mddft.py
@@ -0,0 +1,26 @@
+#! python
+
+from snowwhite.mddftsolver import *
+import numpy as np
+import sys
+
+dims = [32,32,32]
+dimsTuple = tuple(dims)
+k = 1
+
+    
+p1 = MddftProblem(dims, k)
+s1 = MddftSolver(p1, {SW_OPT_CUDA : False})
+
+src = np.ones(dimsTuple, complex)
+for  k in range (np.size(src)):
+    vr = np.random.random()
+    vi = np.random.random()
+    src.itemset(k,vr + vi * 1j)
+
+dstP = s1.runDef(src)
+dstC = s1.solve(src)
+
+diff = np.max ( np.absolute ( dstC - dstP ) )
+
+print ('Diff between Python/C transforms = ' + str(diff) )
