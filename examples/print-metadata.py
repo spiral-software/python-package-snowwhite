348729c74513b0c4a45954f49ead22f39f1281d7
Mike Franusich <mike.franusich@spiralgen.com>
2022-09-08 09:58:49 -0400

    print-metadata example uses SW_LIBRARY_PATH

--- examples/print-metadata.py
+++ examples/print-metadata.py
@@ -1,4 +1,5 @@
 '''Print meta data in shared library files in snowwhite .libs directory
+   and in directories specified in SW_LIBRARY_PATH
 '''
 
 import os
@@ -8,5 +9,17 @@ from pprint import *
 
 moduleDir = os.path.dirname(os.path.realpath(snowwhite.metadata.__file__))
 libsDir = os.path.join(moduleDir, SW_LIBSDIR)
-md = metadataInDir(libsDir)
+
+dirlist = [libsDir]
+
+libpath = os.getenv(SW_LIBRARY_PATH)
+if libpath != None:
+    sep = ';' if sys.platform == 'win32' else ':'
+    paths = libpath.split(sep)
+    dirlist = dirlist + paths
+
+md = []
+for libdir in dirlist:    
+    md = md + metadataInDir(libdir)
+
 pprint(md)
